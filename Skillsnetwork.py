bc2c4dfb398ea34273440a26b82c4736937ddf5b
skennedybda <67512861+skennedybda@users.noreply.github.com>
2020-12-02 00:01:27 +0000

    Add files via upload

---
+++ Skillsnetwork.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Scott Kennedy**  
+# Data Analyst
+
+# *I am interested in data science because I would like progress my career.*
+
+# ### The code below will compute a simple mathematical equation that will result in zero.
+
+# In[3]:
+
+
+n=1
+n + n - 2*n
+
+
+# <https://www.cognitiveclass.ai>
+# ---
+# 1. First ordered list item
+# 2. Another item
